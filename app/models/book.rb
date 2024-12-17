5bc3c35a84e0772a7a9959dbb4d3f63e1ef58d1c
Aliyah Moore <aliyahm.moore@yahoo.com>
2024-12-16 22:20:54 -0600

    adjusted the books model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,12 @@
 class Book < ApplicationRecord
   belongs_to :user
   belongs_to :book_club, optional: true
-  attribute :image_link, :string
+  attribute :google_id, :string
+  attribute :image_url, :string
+  attribute :description, :text
   validates :title, presence: true
   validates :author, presence: true
   validates :genre, presence: true
+  validates :status, presence: true
   validates :google_id, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
     devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
     has_many :books
     has_many :book_clubs
-    has_many :notes
 
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
